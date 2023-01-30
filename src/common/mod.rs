e544f9359111310e2f231427eec7232589a93878
Konkitoman <konkitoman@users.noreply.github.com>
2023-01-30 13:55:34 +0200

    Fix a path problem when geting the modules

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -14,7 +14,7 @@ pub use windows::*;
 
 pub fn get_modules() -> Vec<PathBuf> {
     let mut modules = Vec::new();
-    for paths in get_muzzman_dir().read_dir().unwrap() {
+    for paths in get_muzzman_dir().join("modules").read_dir().unwrap() {
         let entry = paths.unwrap();
         let path = entry.path();
         if path
