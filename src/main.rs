c9437afa18a1462b862b6ce341b869454329dc62
Konkitoman <konkitoman@users.noreply.github.com>
2023-02-22 12:49:58 +0200

    Changelog 0.0.8  - daemon now loads all avalibile modules!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
-use muzzman_daemon::daemon::Daemon;
+use muzzman_daemon::{
+    common::get_muzzman_dir, daemon::Daemon, prelude::TModuleInfo, DaemonSession,
+};
 
 fn main() {
     let daemon = Daemon::new().unwrap();
-    daemon.run();
+    let daemon = std::thread::spawn(|| daemon.run());
+    let session = DaemonSession::new()
+        .expect("Some thing went rong!")
+        .create_session();
+    if let Ok(dir) = get_muzzman_dir().join("modules").read_dir() {
+        for file in dir {
+            if let Ok(file) = file {
+                let path = file.path();
+                if let Some(name) = file.file_name().to_str() {
+                    if name.contains(std::env::consts::DLL_EXTENSION) {
+                        match session.load_module(path.clone()) {
+                            Ok(module) => {
+                                println!("Loaded module: {}", module.get_name().unwrap());
+                            }
+                            Err(err) => {
+                                eprintln!("Error when loading: {:?}\n{:?}\n\n", path, err);
+                            }
+                        }
+                    }
+                } else {
+                    eprintln!("Cannot get the file_name for {:?}", path)
+                }
+            } else {
+                eprintln!("Cannot get the file");
+            }
+        }
+    }
+
+    daemon.join().unwrap();
 }
